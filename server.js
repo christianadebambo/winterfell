1fed20fee56223933619edeea914bd9aa2184124
Christian Adebambo <c.adebambo@alustudent.com>
2023-12-15 15:25:59 +0400

    make it better

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 
 // Configure session handling with express-session
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: "rAnDoMdEv",
   resave: false,
   saveUninitialized: false,
   cookie: {
